9c42cb3c5fd2ef74e18f48742f04b973d260d96a
SukhNoor666 <noorchahal2004@gmail.com>
2025-06-02 10:17:04 -0600

    Update

--- tny_script.js
+++ tny_script.js
@@ -4,8 +4,16 @@
    Tutorial Case
 
    Countdown Clock
-   Author: 
-   Date:   
+   Author: Sukhnoor Singh
+   Date:  02-06-2025
 
 */
 
+//Displays current date & time
+document.getElementById("dateNow").innerHTML = 'm/d/y<br /> h:m:s'
+
+// Dislpay the time left until New Years Eve Bash
+document.getElementById("days").textContent = `dd`;
+document.getElementById("hrs").textContent = `hh`;
+document.getElementById("mins").textContent = `mm`;
+document.getElementById("secs").textContent = `ss`;
