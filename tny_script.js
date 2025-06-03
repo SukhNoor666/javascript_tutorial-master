9f7f9800b6a7d5f644db2c5df89f30314a1fbaa5
SukhNoor666 <noorchahal2004@gmail.com>
2025-06-03 11:19:17 -0600

    Update

--- tny_script.js
+++ tny_script.js
@@ -26,3 +26,12 @@ var timeStr = currentDay.toLocaleTimeString();
 //Displays current date & time
 document.getElementById("dateNow").innerHTML = 
 dateStr + "<br />" + timeStr;
+
+//Calculate the days until New Year's Bash
+var newYear = new Date('January 1st, 2025')
+var nextYear = currentDay.getFullYear() + 1;
+newYear.setFullYear(nextYear);
+var daysLeft = (newYear - currentDay)/(1000*60*60);
+
+//Display time left until New Years Eve
+document.getElementById("days").textContent = daysLeft;
