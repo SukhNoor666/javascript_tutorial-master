b29c3ca01fe65737f60ae12b69ea30769a6428f3
SukhNoor666 <noorchahal2004@gmail.com>
2025-06-04 11:06:22 -0600

    Update

--- tny_script.js
+++ tny_script.js
@@ -8,8 +8,10 @@
    Date:  02-06-2025
 
 */
+// Invoking the function defined above.
+setInterval("runClock()", 1000);
 
-
+function runClock(){
 
 // Dislpay the time left until New Years Eve Bash
 document.getElementById("days").textContent = `dd`;
@@ -22,28 +24,29 @@ var currentDay = new Date();
 var dateStr = currentDay.toLocaleDateString();
 var timeStr = currentDay.toLocaleTimeString();
 
-
 //Displays current date & time
-document.getElementById("dateNow").innerHTML = 
-dateStr + "<br />" + timeStr;
-
+function currentDay(dateStr, timeStr) {
+  document.getElementById("dateNow").innerHTML = dateStr + "<br />" + timeStr;
+}
 //Calculate the days until New Year's Bash
-var newYear = new Date('January 1st, 2025')
+var newYear = new Date("January 1st, 2025");
 var nextYear = currentDay.getFullYear() + 1;
 newYear.setFullYear(nextYear);
-var daysLeft = (newYear - currentDay)/(1000*60*60*24);
+var daysLeft = (newYear - currentDay) / (1000 * 60 * 60 * 24);
 
 // Calculate the hours left in the current day
 var hrsLeft = (daysLeft - Math.floor(daysLeft)) * 24;
 
 // Calculate the minutes left in the current day
-var minsLeft = (hrsLeft - Math.floor(hrsLeft)) * 60; 
+var minsLeft = (hrsLeft - Math.floor(hrsLeft)) * 60;
 
 // Calculate the seconds left in the current day
-var secsLeft = (minsLeft - Math.floor(minsLeft)) * 60; 
+var secsLeft = (minsLeft - Math.floor(minsLeft)) * 60;
 
 //Display time left until New Years Eve
 document.getElementById("days").textContent = Math.floor(daysLeft);
 document.getElementById("hrs").textContent = Math.floor(hrsLeft);
 document.getElementById("mins").textContent = Math.floor(minsLeft);
 document.getElementById("secs").textContent = Math.floor(secsLeft);
+}
+
